b23b885150122722e002fdd7ece0ed1b06386f83
Rayan Sazawa Rai <yesnoimpro@gmail.com>
2024-02-13 20:17:24 +0545

    Added minecraft cleint and keyboard mixins

---
+++ src/main/java/com/withtutorial/mixin/KeyboardMixin.java
@@ -0,0 +1,18 @@
+package com.withtutorial.mixin;
+
+import net.minecraft.client.Keyboard;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.withtutorial.MujiMod;
+
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+    @Inject(at = @At("HEAD"), method = "onKey()V")
+    public void _onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci)
+    {
+        MujiMod.INSTANCE.onKey(key, action);
+    }
+}
